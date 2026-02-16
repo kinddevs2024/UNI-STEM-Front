f13b459f50ef59f64a9d12bcf07f187d9b8d5739
kinddevs2024 <kinddevs2024@gmail.com>
2026-02-16 17:31:21 +0500

    a

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13,7 +13,7 @@ export const getImageUrl = (url) => {
 
   const apiBase = typeof API_BASE_URL === "string" && API_BASE_URL.trim()
     ? API_BASE_URL.replace(/\/$/, "")
-    : "http://173.249.47.147api";
+    : "https://kinddevs2024-global-olimpiad-v2-2-b.vercel.app/api";
 
   // If it starts with /, construct full URL using the API base domain
   if (url.startsWith('/')) {
